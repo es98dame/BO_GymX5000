0d4b7ed7f51e7b5a27ca9364e8d1ec392b74eac6
DamiKim <dmkr.kim@gmail.com>
2022-11-30 05:32:37 -0700

    updated package.json

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import App from './components/App.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
+require('bootstrap/dist/css/bootstrap.css');
 import {AuthProvider} from './components/context/Auth.jsx';
 const root = createRoot(document.getElementById('root'));
 
