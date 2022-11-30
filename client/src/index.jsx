52899817b17ea3d2e73d459eb98235ca31dd2014
DamiKim <dmkr.kim@gmail.com>
2022-11-30 06:10:14 -0700

    added css loader

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import App from './components/App.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/public/css/bootstrap.min.css';
 import {AuthProvider} from './components/context/Auth.jsx';
 const root = createRoot(document.getElementById('root'));
 
