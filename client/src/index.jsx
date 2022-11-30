6db6d917a9f57bba9c5408831241d75c3b04b57a
DamiKim <dmkr.kim@gmail.com>
2022-11-30 05:42:32 -0700

    updated css rul

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import App from './components/App.jsx';
-require('bootstrap/dist/css/bootstrap.css');
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {AuthProvider} from './components/context/Auth.jsx';
 const root = createRoot(document.getElementById('root'));
 
